08cb89381cd3adf8b14cf52249a8e0d0a2525f88
paddy <paddy.rentomed@gmail.com>
2020-12-11 17:30:28 +0530

    initial git

---
+++ loginapp/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+# Create your models here.
+
+class Emp(models.Model):
+    eno=models.IntegerField()
+    ename=models.CharField(max_length=100)
+    esal=models.IntegerField()
